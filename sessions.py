209baa2f7c1dffd751fc318dc70ff21b9c272055
Md. Sifat Kamal <md.sifat.kamal@g.bracu.ac.bd>
2025-08-07 17:29:54 +0600

    Accesed to MyAttendance

--- sessions.py
+++ sessions.py
@@ -1,6 +1,10 @@
 from appium import webdriver
 from appium.options.android import UiAutomator2Options
 from os import path
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 
 CUR_DIR = path.dirname(path.abspath(__file__))
 
@@ -14,6 +18,7 @@ CAPS = {
     'deviceName': 'Android Emulator',
     'automationName': 'UIAutomator2',
     'app': APP,
+    "autoGrantPermissions": True
 }
 
 options = UiAutomator2Options().load_capabilities(CAPS)
@@ -23,4 +28,32 @@ driver = webdriver.Remote(
     options=options
 )
 
-driver.quit()
+try:
+
+    wait = WebDriverWait(driver, 10)
+
+    wait.until(EC.presence_of_element_located(
+
+        (AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Enter Email")')
+
+    ))
+
+    driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Enter Email")').send_keys('azmin@excelbd.com')
+
+    driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Enter Password")').send_keys('D!m77(2SJ,5j')
+
+    driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Login")').click()
+
+    wait.until(EC.presence_of_element_located(
+
+        (AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.ImageView").instance(2)')
+
+    ))
+
+    myAttendance = driver.find_element(AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.widget.ImageView").instance(2)')
+
+    myAttendance.click()
+
+except:
+
+    driver.quit()
